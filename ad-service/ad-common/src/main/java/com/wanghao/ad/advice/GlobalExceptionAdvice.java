64bf2d943c0d772de51f32627bac873f868d9f0a
winner <1750110604@qq.com>
2020-03-07 21:47:56 +0800

    统一响应，统一异常

---
+++ ad-service/ad-common/src/main/java/com/wanghao/ad/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,18 @@
+package com.wanghao.ad.advice;
+
+import com.wanghao.ad.exception.AdException;
+import com.wanghao.ad.vo.CommonResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler(value = AdException.class)
+    public CommonResponse<String> handlerAdException(HttpServletRequest req, AdException ex) {
+        CommonResponse<String> response = new CommonResponse<>(-1, "business error");
+        response.setData(ex.getMessage());
+        return response;
+    }
+}
