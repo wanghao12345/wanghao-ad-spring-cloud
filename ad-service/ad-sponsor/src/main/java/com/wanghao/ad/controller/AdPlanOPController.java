22d2ef7d51f35fa88ddfcb98036f362f4186fe0c
wanghao <1750110604@qq.com>
2020-03-25 19:05:08 +0800

    controller

--- ad-service/ad-sponsor/src/main/java/com/wanghao/ad/controller/AdPlanOPController.java
+++ ad-service/ad-sponsor/src/main/java/com/wanghao/ad/controller/AdPlanOPController.java
@@ -1,8 +1,15 @@
 package com.wanghao.ad.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.wanghao.ad.entity.AdPlan;
+import com.wanghao.ad.exception.AdException;
 import com.wanghao.ad.service.IAdPlanService;
+import com.wanghao.ad.vo.AdPlanRequest;
+import com.wanghao.ad.vo.AdPlanResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -14,5 +21,11 @@ public class AdPlanOPController {
     @Autowired
     public AdPlanOPController(IAdPlanService adPlanService) {
         this.adPlanService = adPlanService;
+    }
+
+    @PostMapping("/create/adPlan")
+    public AdPlanResponse createAdPlan(@RequestBody AdPlanRequest request) throws AdException {
+        log.info("ad-sponsor: createAdPlan -> {}", JSON.toJSONString(request));
+        return adPlanService.createAdPlan(request);
     }
 }
